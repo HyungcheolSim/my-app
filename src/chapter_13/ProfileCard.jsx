f6fc8e131182fbc078a8619fdb6322355322c701
HyungcheolSim <sim34122@gmail.com>
2024-11-18 21:55:37 +0900

    Composition vs Inheritance

---
+++ src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props){
+    return (
+        <Card title= "HyeongCheol Sim" backgroundColor="#4ea04e">
+            <p>안녕하세요, 심형철입니다.</p>
+            <p>저는 리액트를 사용해서 개발하고 있습니다.</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
